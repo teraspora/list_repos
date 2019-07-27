6e59b29d8bdccb333ee57bae3e4dc24427a04d1c
John Lynch <johnlynch.32768@gmail.com>
2019-07-27 06:39:49 +0100

    Fix colours, spacing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # List a User's Github repositories (names and URLs)
 
-A simple Python script to get a JSON file listing a given user's repositories using the Github API, and listing the names and URLs in the terminal.
+A simple Python script to get a JSON file listing a given user's repositories 
+(well, the first 1000 of them) using the Github API, and listing the names and URLs in the terminal.)
 
 With most terminal applications, of course, these URLs can be Ctrl+click-ed / Command+click-ed to open them in a new page of the default browser.
--- list_repos.py
+++ list_repos.py
@@ -6,10 +6,14 @@
 
 import json, urllib.request
 
+print('List a user\'s Github repositories...')
 username = input('\u001B[91mEnter username:\n    \u001B[96m')
-print('\u001B[91m\n')
-url = f'https://api.github.com/users/{username}/repos'
-repos = json.loads(urllib.request.urlopen(url).read())
+print('\u001B[91m')
+urls = [f'https://api.github.com/users/{username}/repos?per_page=100&page={i}' for i in range(1, 11)]
 
-for repo in repos:
-    print(f'{repo["name"]} - {repo["html_url"]}')
+print(f'\u001B[33mGithub Repositories for {username}:\u001B[91m\n')
+
+for url in urls:
+    repos = json.loads(urllib.request.urlopen(url).read())
+    for repo in repos:
+        print(f'{repo["name"]} - {repo["html_url"]}')
